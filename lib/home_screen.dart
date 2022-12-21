bd133dbd7306282d3ea589c861e1e263deb0a22f
Nguyen Phuc Son Tung <ngphsotu@gmail.com>
2022-12-22 04:03:08 +0700

    completed

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,13 +1,90 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  var currentIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(),
-      body: const Text('Hello World'),
+      bottomNavigationBar: bottomNavigationBar(size),
     );
   }
+
+  bottomNavigationBar(Size size) {
+    return Container(
+      margin: const EdgeInsets.all(20),
+      height: size.width * .155,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(.15),
+            blurRadius: 30,
+            offset: const Offset(0, 10),
+          ),
+        ],
+        borderRadius: BorderRadius.circular(50),
+      ),
+      child: ListView.builder(
+        itemCount: 4,
+        scrollDirection: Axis.horizontal,
+        padding: EdgeInsets.symmetric(horizontal: size.width * 0.024),
+        itemBuilder: (context, index) => InkWell(
+          onTap: () {
+            setState(
+              () {
+                currentIndex = index;
+              },
+            );
+          },
+          splashColor: Colors.transparent,
+          highlightColor: Colors.transparent,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              AnimatedContainer(
+                duration: const Duration(milliseconds: 1500),
+                curve: Curves.fastLinearToSlowEaseIn,
+                margin: EdgeInsets.only(
+                  bottom: index == currentIndex ? 0 : size.width * .029,
+                  right: size.width * .0422,
+                  left: size.width * .0422,
+                ),
+                width: size.width * .128,
+                height: index == currentIndex ? size.width * .014 : 0,
+                decoration: const BoxDecoration(
+                  color: Colors.blueAccent,
+                  borderRadius: BorderRadius.vertical(
+                    bottom: Radius.circular(10),
+                  ),
+                ),
+              ),
+              Icon(
+                listOfIcons[index],
+                size: size.width * .076,
+                color:
+                    index == currentIndex ? Colors.blueAccent : Colors.black38,
+              ),
+              SizedBox(height: size.width * .03),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  List<IconData> listOfIcons = [
+    Icons.home_rounded,
+    Icons.favorite_rounded,
+    Icons.settings_rounded,
+    Icons.person_rounded,
+  ];
 }
